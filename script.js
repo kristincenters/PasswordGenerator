7c540c95bfec7f6ce7dd574af25a198dd009b586
kcenters <kristincenters@gmail.com>
2020-02-23 15:32:43 -0600

    corrected array syntax

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // defined arrays
-var upperCaseLtrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var upperCaseLtrs = [ABCDEFGHIJKLMNOPQRSTUVWXYZ];
 var lowerCaseLtrs = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var specialChars = ["!","@","#","$","%","^","&","*","(",")"];
 var numerals = ["0","1","2","3","4","5","6","7","8"];
